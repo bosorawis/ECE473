3dcacb1ade358382e36a36bc9b1bfd011d131cae
dihmuzikien <turbo2547@hotmail.com>
2015-10-10 18:00:00 -0700

    started project

---
+++ labs/lab2/buttontest.c
@@ -0,0 +1,73 @@
+// testled1.c
+// R. Traylor
+// 10.3.05
+// Tests wiring of LED board to mega128.
+// Select a digit with PORTB upper nibble then with
+// port D push buttons illuminate a single segment.
+
+// Port mapping:
+// Port A:  bit0 brown  segment A
+//          bit1 red    segment B
+//          bit2 orange segment C
+//          bit3 yellow segment D
+//          bit4 green  segment E
+//          bit5 blue   segment F
+//          bit6 purple segment G
+//          bit7 grey   decimal point
+//               black  Vdd
+//               white  Vss
+
+// Port B:  bit4 green  seg0
+//          bit5 blue   seg1
+//          bit6 purple seg2
+//          bit7 grey   pwm 
+//               black  Vdd
+//               white  Vss
+
+#include <avr/io.h>
+static uint16_t state = 0; //holds present state
+
+int8_t debounce_switch(){
+    state = (state << 1) | (! bit_is_clear(PINA, 0)) | 0xE000;
+    if (state == 0xF000) return 1;
+    return 0;
+}
+
+uint8_t chk_buttons(uint8_t button){
+    static uint16_t state = 0; //holds present state
+    state = (state << 1) | (! bit_is_clear(PINA, button)) | 0xE000;
+    if (state == 0xF000) {
+        return 1;
+    }
+    return 0;   
+}
+int main()
+{
+    DDRA  = 0x00;   //set port A to all Inputs
+    DDRB  = 0xF0;   //set port bits 4-7 B as outputs
+    DDRD  = 0x00;   //set port D all inputs 
+    PORTD = 0xFF;   //set port D all pullups 
+    //PORTA = 0xFF;   //set port A to all ones  (off, active low)
+
+    while(1){
+	PORTB = 0x70; // Turn button board on
+	DDRA = 0x00;  //PortA input mode
+	PORTA = 0xFF;  // PORTA input mode
+	__asm__ __volatile__ ("nop"); //buffer
+	__asm__ __volatile__ ("nop"); //buffer
+	if (chk_buttons(0)){ //If a button is pressed
+	    DDRA = 0xFF; //PortA output
+	    __asm__ __volatile__ ("nop");
+	    __asm__ __volatile__ ("nop");
+	    PORTA = 0x00; //LED on
+	    PORTB = 0x00; // Selector 	
+	} 
+	else if (debounce_switch()){ //If a button is pressed
+	    DDRA = 0xFF; //PortA output
+	    __asm__ __volatile__ ("nop");
+	    __asm__ __volatile__ ("nop");
+	    PORTA = 0x00; //LED on
+	    PORTB = 0x10; // Selector 	
+	} 
+    } //while
+}  //main
--- labs/lab2/lab2_skel.c
+++ labs/lab2/lab2_skel.c
@@ -11,14 +11,17 @@
 #define F_CPU 16000000 // cpu speed in hertz 
 #define TRUE 1
 #define FALSE 0
+#define INPUT 0xFF
+#define SELECT_BIT_BUTTON_BOARD 0x70
 #include <avr/io.h>
 #include <util/delay.h>
 
+
 //holds data to be sent to the segments. logic zero turns segment on
-uint8_t segment_data[5] 
+uint8_t segment_data[5];
 
 //decimal to 7-segment LED display encodings, logic "0" turns on segment
-uint8_t dec_to_7seg[12] 
+uint8_t dec_to_7seg[12];
 
 
 //******************************************************************************
@@ -31,18 +34,23 @@ uint8_t dec_to_7seg[12]
 //external loop delay times 12. 
 //
 uint8_t chk_buttons(uint8_t button) {
-//******************************************************************************
+    //******************************************************************************
+    static uint16_t state = 0; //holds present state
+    state = (state << 1) | (! bit_is_clear(PIND, 0)) | 0xE000;
+    if (state == 0xF000) return 1;
+    return 0;
 
+}
 //***********************************************************************************
 //                                   segment_sum                                    
 //takes a 16-bit binary input value and places the appropriate equivalent 4 digit 
 //BCD segment code in the array segment_data for display.                       
 //array is loaded at exit as:  |digit3|digit2|colon|digit1|digit0|
 void segsum(uint16_t sum) {
-  //determine how many digits there are 
-  //break up decimal sum into 4 digit-segments
-  //blank out leading zero digits 
-  //now move data to right place for misplaced colon position
+    //determine how many digits there are 
+    //break up decimal sum into 4 digit-segments
+    //blank out leading zero digits 
+    //now move data to right place for misplaced colon position
 }//segment_sum
 //***********************************************************************************
 
@@ -50,18 +58,19 @@ void segsum(uint16_t sum) {
 //***********************************************************************************
 uint8_t main()
 {
-//set port bits 4-7 B as outputs
-while(1){
-  //insert loop delay for debounce
-  //make PORTA an input port with pullups 
-  //enable tristate buffer for pushbutton switches
-  //now check each button and increment the count as needed
-  //bound the count to 0 - 1023
-  //break up the disp_value to 4, BCD digits in the array: call (segsum)
-  //bound a counter (0-4) to keep track of digit to display 
-  //make PORTA an output
-  //send 7 segment code to LED segments
-  //send PORTB the digit to display
-  //update digit to display
-  }//while
+    //set port bits 4-7 B as outputs
+    DDRB = 0x70;
+    while(1){
+	//insert loop delay for debounce 
+	//make PORTA an input port with pullups 
+	//enable tristate buffer for pushbutton switches
+	//now check each button and increment the count as needed
+	//bound the count to 0 - 1023
+	//break up the disp_value to 4, BCD digits in the array: call (segsum)
+	//bound a counter (0-4) to keep track of digit to display 
+	//make PORTA an output
+	//send 7 segment code to LED segments
+	//send PORTB the digit to display
+	//update digit to display
+    }//while
 }//main
